1c7fbb826f008716ec48c355486d8e8335b60275
eriksvedenlund <eriksvedenlund@gmail.com>
2018-01-05 19:40:27 +0100

    testing some api calls

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 export default class App extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			topGames: []
+		}
+	}
+
+	getStreams = () => {
+		const url = 'https://api.twitch.tv/kraken/games/top/'
+		axios.get(url, {
+			headers: {
+	   			'Client-ID': 'aaf1nw0m0glpzetrm6ddc0vto6ll7f'
+	 			}
+	 		})
+			.then(res => {
+				this.setState({
+					topGames: res.data.top
+				});
+			})
+		.catch(err => console.error(err))
+	}
+
 	render() {
+		const topGamesData = this.state.topGames.map((item, index) => {
+			return(
+				<div>
+					<p>{item.viewers}</p>
+					<p>{item.game.name}</p>
+				</div>
+			);
+		});
 		return(
 			<div>
-				<h1>hello twitch</h1>
+				<button onClick={this.getStreams}>Get the stuff</button>
+				{topGamesData}
 			</div>
 		);
 	}
